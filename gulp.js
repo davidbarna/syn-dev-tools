28193b52adde724234327ea8d933bc4854544f5b
David Almeida (Soyun) <david.almeida@soyun.es>
2016-02-23 13:56:47 +0100

    coffeescript support added to gulp services

--- gulp.js
+++ gulp.js
@@ -1,1 +1,8 @@
+/*
+ * dev-tools gulp services cannot be packaged with main js file as it must be
+ * included only for dev purpose in a node environment (not browerify).
+ * That's why a separate entry point was created with coffeescript
+ * auto compilation
+ */
+require( 'coffee-script/register' );
 module.exports = require( './src/gulp' )
